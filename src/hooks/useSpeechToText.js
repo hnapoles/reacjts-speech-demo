e94964d70c6dd3132fc6b2ed25e81b2240bc4e66
Meriam Pales <meriampdev@gmail.com>
2023-03-16 18:48:23 +0800

    pwa

--- src/hooks/useSpeechToText.js
+++ src/hooks/useSpeechToText.js
@@ -8,16 +8,16 @@ export const useSpeechToText = () => {
   useEffect(() => {
     let SpeechRecognition =
       window.webkitSpeechRecognition || window.SpeechRecognition
-    let SpeechGrammarList =
-      window.SpeechGrammarList || window.webkitSpeechGrammarList
-    let moods = ['happy', 'sad', 'sleepy', 'angry']
-    let grammar =
-      '#JSGF V1.0; grammar moods; public <moods> = ' + moods.join(' | ') + ';'
+    // let SpeechGrammarList =
+    //   window.SpeechGrammarList || window.webkitSpeechGrammarList
+    // let moods = ['happy', 'sad', 'sleepy', 'angry']
+    // let grammar =
+    //   '#JSGF V1.0; grammar moods; public <moods> = ' + moods.join(' | ') + ';'
 
     let _recognition = new SpeechRecognition()
-    let recognitionList = new SpeechGrammarList()
-    recognitionList.addFromString(grammar, 1)
-    _recognition.grammars = recognitionList
+    // let recognitionList = new SpeechGrammarList()
+    // recognitionList.addFromString(grammar, 1)
+    // _recognition.grammars = recognitionList
     _recognition.lang = 'en-US'
     _recognition.continuous = false
     _recognition.interimResults = false
